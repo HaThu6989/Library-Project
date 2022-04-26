8e61d6afc343e5e7584e2685923c1f8048ddc62a
Ha Thu <tranhathu6989@gmail.com>
2022-04-26 09:51:59 +0200

    delete book

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,75 +1,114 @@
 const Book = require("../models/Book.model");
-//const Book = model("Book", bookSchema);
 
 const router = require("express").Router();
 
-// TASK
-// 
-// when user visits localhost:3000/books
-// 
-// - send query to DB to get list of books
-// - if query is successful:
-//   --> console.log the list of books + res.send("great news!")
-// 
-// 
 
+// READ: display list of books
 router.get("/books", (req, res, next) => {
   Book.find()
     .then((booksArr) => {
-      // console.log(booksArr[0].title);
-      // res.send("great news!");
-      res.render("books/books-list", { books: booksArr })
+      res.render("books/books-list", { books: booksArr });
     })
     .catch(err => {
-      console.log("error getting books from DB ", err);
+      console.log("error getting books from DB", err)
       next(err);
-    })
-
+    });
 });
 
-//Phải đặt trc router.get("/books/:bookId", (req, res, next), vì kế thừ lại code 
+
+// CREATE: render form
 router.get("/books/create", (req, res, next) => {
-  res.render("books/book-create")
+  res.render("books/book-create");
 })
 
 
-
-router.get("/books/:bookId", (req, res, next) => {
-  const id = req.params.bookId;
-  Book.findById(id)
-    .then(bookDetails => {
-      console.log(bookDetails);
-      res.render("books/book-details", { book: bookDetails });
-
-    })
-    .catch(err => {
-      console.log("error getting book datails from DB", err);
-      next(err)
-    })
-})
-
-
-
+// CREATE: process form
+//"/books/create" luon dung trc "/books/:bookId" : router.get("/books/:bookId", (req, res, next)
+//neu ko => error
 router.post("/books/create", (req, res, next) => {
-  console.log(req.body); //{title: book new,...}
+
   const newBook = {
     title: req.body.title,
-    author: req.body.author,
     description: req.body.description,
+    author: req.body.author,
     rating: req.body.rating,
-  };
+  }
 
   Book.create(newBook)
-    .then(bookFromDB => {
-      res.redirect("/books"); //redirect to the products page
-      // console.log(newProduct)
+    .then((bookFromDB) => {
+      res.redirect("/books");
     })
     .catch(err => {
-      console.log("error creating new book on DB", err);
-      next(err)
+      console.log("error creating book on DB", err)
+      next(err);
+    });
+
+})
+
+
+// READ: display book details
+router.get("/books/:bookId", (req, res, next) => {
+  const id = req.params.bookId;
+
+  Book.findById(id)
+    .then((bookDetails) => {
+      res.render("books/book-details", bookDetails);
+    })
+    .catch(err => {
+      console.log("error getting book details from DB", err)
+      next(err);
     });
 })
 
 
+// UPDATE: display form
+router.get("/books/:bookId/edit", (req, res, next) => {
+  const id = req.params.bookId;
+  Book.findById(id)
+    .then((bookDetails) => {
+      res.render("books/book-edit", bookDetails);
+    })
+    .catch(err => {
+      console.log("error updating book from DB", err)
+      next(err);
+    });
+});
+
+
+
+// UPDATE: process form
+router.post("/books/:bookId/edit", (req, res, next) => {
+
+  const id = req.params.bookId;
+
+  const newDetails = {
+    title: req.body.title,
+    description: req.body.description,
+    author: req.body.author,
+    rating: req.body.rating,
+  };
+
+  Book.findByIdAndUpdate(id, newDetails)
+    .then((bookFromDB) => {
+      res.redirect(`/books/${bookFromDB._id}`);
+    })
+    .catch(err => {
+      console.log("error updating book in DB", err)
+      next(err);
+    });
+});
+
+//DELETE
+router.post("/books/:bookId/delete", (req, res, next) => {
+  const id = req.params.bookId;
+  Book.findByIdAndRemove(id,)
+    .then((response) => {
+      res.redirect("/books/");
+    })
+    .catch(err => {
+      console.log("error deleting book in DB", err)
+      next(err);
+    });
+})
 
 module.exports = router;
